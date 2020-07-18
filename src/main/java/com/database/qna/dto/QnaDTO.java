240b85c717fbd0f359b998f36f2e572f0e3e5063
Kim SeokGeun <djdjwing@naver.com>
2020-07-18 19:54:25 +0900

    QNA 수정 1차(7/18)

--- src/main/java/com/database/qna/dto/QnaDTO.java
+++ src/main/java/com/database/qna/dto/QnaDTO.java
@@ -1,5 +1,21 @@
 package com.database.qna.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class QnaDTO {
+
+    private int qna_no;
+    private int desc;
+    private String qna_title;
+    private String qan_content;
+    private String qna_regdate;
+    private int fk_member_no;
+    private int qna_reply_cnt;
+    private String writer;
 
 }
--- src/main/java/com/database/qna_reply/dto/Qna_repDTO.java
+++ src/main/java/com/database/qna_reply/dto/Qna_repDTO.java
@@ -1,5 +1,17 @@
 package com.database.qna_reply.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Qna_repDTO {
+
+    private int q_reply_no;
+    private int q_reply_qna_no;
+    private String q_reply_content;
+    private String q_reply_regdate;
 
 }
