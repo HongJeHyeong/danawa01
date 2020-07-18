979ecf6f49380e57c2b2b5b23075dfea0e679d14
swsw <swsw1005@gmail.com>
2020-07-18 18:23:05 +0900

    @service 추가 및 bean 등록

---
+++ src/main/java/com/database/board/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.database.board.service;
+
+import com.database.board.dao.BoardDAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+  @Autowired
+  private BoardDAO boardDAO;
+  /////////// ------- 이 아래로 작성하세요 -------////////////////////////////
+
+}
