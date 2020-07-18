077f242cc4f936dfccad4969b7998c9d93fb661d
swsw <swsw1005@gmail.com>
2020-07-19 02:37:43 +0900

    mock Test 관련, @Alias 세팅

--- src/main/java/com/database/member/dto/MemberDTO.java
+++ src/main/java/com/database/member/dto/MemberDTO.java
@@ -2,6 +2,8 @@ package com.database.member.dto;
 
 import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
 import lombok.ToString;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @ToString
+@Alias("memberDTO")
 public class MemberDTO {
 	private int member_no,member_grade;
 	private String member_id,member_pw,member_name,member_email,member_tel1,member_tel2,member_tel3,member_addr,member_addr2;
